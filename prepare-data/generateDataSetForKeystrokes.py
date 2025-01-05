a2bef0df631b46b8f47b8a8fc40cb23e1354aa9b
Disha Modi <bhavnamodi802@gmail.com>
2025-01-06 02:05:41 +0530

    [Data Cleaning] : Multithreading implemented to improve the processing of data

--- prepare-data/generateDataSetForKeystrokes.py
+++ prepare-data/generateDataSetForKeystrokes.py
@@ -1,37 +1,57 @@
+import threading
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import re
 from pathlib import Path
 
-FOLDER_PATH = "/home/dishamodi0910/DEV/true-identify/Keystrokes/files";
-result = []
-def generateDataSet(filepath):
-    global result
-    with open(filepath, encoding='ISO-8859-1', errors='ignore') as file_content:
-        for line in file_content:
-            #print(f"Raw line: {repr(line)}")  
-            matches = line.split("\t")
-            print(line)
-            if(matches[0] and matches[1] and matches[-2] and matches[-3] and matches[-4]):
-            # print(matches[0])
-            # print(matches[1])
-            # print(matches[-2])
-            # print(matches[-3])
-            # print(matches[-4])
-            # print(matches)  
-                result.append({"participant_id" : matches[0], "test_section_id" : matches[1], "key_used" : matches[-2], "press_time" : matches[-4], "release_time" : matches[-3]})
+FOLDER_PATHS = [ 
+                    "/home/dishamodi0910/DEV/true-identify/Keystroke_0/keystroke_0_0",
+                    "/home/dishamodi0910/DEV/true-identify/Keystroke_0/keystroke_0_1",
+               ];
 
-
-def processAllFilesInFolder():
-    folder = Path(FOLDER_PATH)
+def generateDataSet(folderPath, result_list):
+    folder_data = []
+    folder = Path(folderPath)
     for file_path in folder.iterdir():
         if file_path.is_file():
             print(file_path)
             print("Processing file...")
-            generateDataSet(file_path)
+            with open(file_path, encoding='ISO-8859-1', errors='ignore') as file_content:
+                for line in file_content:
+                    #print(f"Raw line: {repr(line)}")  
+                    matches = line.split("\t")
+                    print(line)
+                    if(matches[0] and matches[1] and matches[-2] and matches[-3] and matches[-4]):
+                        # print(matches[0])
+                        # print(matches[1])
+                        # print(matches[-2])
+                        # print(matches[-3])
+                        # print(matches[-4])
+                        # print(matches)  
+                        folder_data.append({"participant_id" : matches[0], "test_section_id" : matches[1], "key_used" : matches[-2], "press_time" : matches[-4], "release_time" : matches[-3]})
         else:
             print(file_path)
             print("Not a file, hence skipping")
+    result_list.append(folder_data)
 
-processAllFilesInFolder()
+
+def processAllFilesInFolder(folders):
+    threads = []
+    result_list = []
+
+    for folder in folders:
+        thread = threading.Thread(target=generateDataSet, args=(folder, result_list))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    all_data = [item for sublist in result_list for item in sublist]
+    df = pd.DataFrame(all_data)
+    return df
+
+
+df = processAllFilesInFolder(FOLDER_PATHS)
+print(df.head())
---
+++ prepare-data/traceOutParameters.py
@@ -0,0 +1,2 @@
+def generateTheParameters():
+    pass
